cb46626cdd6359a1eba9de6c7c5f4c83231dad81
Vishnu Pratap Singh Rathore <rathorevishnu666@gmail.com>
2026-08-04 23:07:11 +0530

    Create Number_of_Islands.py

---
+++ topics/Number_of_Islands.py
@@ -0,0 +1,33 @@
+def num_islands(grid):
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    count = 0
+
+    def dfs(r, c):
+        # out of bounds or water/already visited -> stop
+        if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != '1':
+            return
+        grid[r][c] = '0'   # mark as visited by "sinking" it
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == '1':
+                count += 1
+                dfs(r, c)   # sink this entire island
+
+    return count
+
+
+grid = [
+    ["1","1","0","0","0"],
+    ["1","1","0","0","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+]
+print(num_islands(grid))   # 3
