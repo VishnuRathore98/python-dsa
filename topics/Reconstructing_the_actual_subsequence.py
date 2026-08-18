4c6aa5863ed06f6f6e9c82f9f006196abf76ea70
Vishnu Pratap Singh Rathore <rathorevishnu666@gmail.com>
2026-08-18 23:15:51 +0530

    Create Reconstructing_the_actual_subsequence.py

---
+++ topics/Reconstructing_the_actual_subsequence.py
@@ -0,0 +1,27 @@
+def lcs_string(text1, text2):
+    m, n = len(text1), len(text2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # backtrack from dp[m][n] to reconstruct the sequence
+    i, j = m, n
+    result = []
+    while i > 0 and j > 0:
+        if text1[i - 1] == text2[j - 1]:
+            result.append(text1[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return "".join(reversed(result))
+
+print(lcs_string("abcde", "ace"))   # "ace"
